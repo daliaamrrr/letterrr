9d9adbb670029914dbf18827af11afae5ff4065a
daliaamrrr <daliaamr2211@gmail.com>
2026-05-13 06:04:54 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('envelopeContainer');
+
+container.addEventListener('click', () => {
+    // This toggles the 'open' class on the container
+    container.classList.toggle('open');
+});
